827e342676603dd5e31fbb5c5b775431a66ae5ca
Hans Toshihide Törnqvist <hans.tornqvist@chalmers.se>
2025-09-11 17:01:40 +0200

    ZeroSuppress node tested.

---
+++ test/test_node_zero_suppress.cpp
@@ -0,0 +1,71 @@
+/*
+ * plutt, a scriptable monitor for experimental data.
+ *
+ * Copyright (C) 2025
+ * Hans Toshihide Toernqvist <hans.tornqvist@chalmers.se>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301  USA
+ */
+
+#include <cstdint>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <node_zero_suppress.hpp>
+#include <test/mock_node.hpp>
+#include <test/test.hpp>
+
+namespace {
+
+class MyTest: public Test {
+  void Run();
+};
+MyTest g_test_node_zero_suppress_;
+
+class MockNode: public MockNodeValue {
+  public:
+    MOCK_NODE_VALUE(MockNode)
+    void ProcessUser()
+    {
+      Input::Scalar s;
+      s.u64 = 1;
+      m_value[0].Push(2, s);
+      s.u64 = 2;
+      m_value[0].Push(3, s);
+    }
+};
+
+void MyTest::Run()
+{
+  MockNode nv(Input::kUint64, 1);
+  NodeZeroSuppress n("", &nv, 1.5);
+
+  auto const &v = n.GetValue(0);
+  TEST_BOOL(v.GetV().empty());
+
+  nv.Preprocess(&n);
+  TestNodeProcess(n, 1);
+
+  TEST_CMP(v.GetID().size(), ==, 1UL);
+  TEST_CMP(v.GetID().at(0), ==, 3UL);
+  TEST_CMP(v.GetEnd().size(), ==, 1UL);
+  TEST_CMP(v.GetEnd().at(0), ==, 1UL);
+  TEST_CMP(v.GetV().size(), ==, 1UL);
+  TEST_CMP(v.GetV().at(0).u64, ==, 2UL);
+}
+
+}
