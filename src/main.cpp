56ad00948fe8af46138908d3f56761737c2bea72
Haakan T Johansson <f96hajo@chalmers.se>
2026-05-26 08:37:13 +0200

    Document the -h option.

--- src/main.cpp
+++ src/main.cpp
@@ -116,6 +116,7 @@ namespace {
     std::cout << "Usage: " << g_arg0 <<
         " -f config [-d path] [-g gui] [-j jobs] input...\n";
     std::cout << "\n";
+    std::cout << " -h   print usage statement.\n";
     std::cout << " -d   generate dot file from nodes.\n";
     std::cout << " -g   values (this arg can be repeated):";
 #if PLUTT_SDL2
